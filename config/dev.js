50a89ddaa69d2485d686aa2a01142dbc29b2eea5
nTorres <ntorres12@icloud.com>
2022-09-03 10:02:51 -0500

    a

--- config/dev.js
+++ config/dev.js
@@ -11,7 +11,7 @@ module.exports = {
   urlPdf: 'http://localhost:3984/',
   // pathSavePdf: '/Users/nelsontorres/Developer/Electrociviles/ati-service-backend/pdf/',
   pathSavePdf: "C://Users//nelson.torres//Desktop//Estudio Nelson//Ati//ati-service-backend//pdf//",
-  pathServicePhp: 'http://localhost/ati-pdf-server/',
+  pathServicePhp: 'http://localhost:8080/ati-pdf-server/',
   jsReportClient: 'http://127.0.0.1:4014/',
   tokenLife: '1hr',
   refreshTokenLife: '50hr'
